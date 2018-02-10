0380414ed45b0d90e271bdab919befe8273b5af8
Eden33 <eduard.gopp@gmail.com>
2018-02-10 20:22:21 +0100

    remove sensitive data from wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -16,18 +16,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', '***REMOVED***');
+define('DB_NAME', '');
 
 /** MySQL database username */
-define('DB_USER', '***REMOVED***');
+define('DB_USER', '');
 
 /** MySQL database password */
-//define('DB_PASSWORD', '***REMOVED***');
-define('DB_PASSWORD', '***REMOVED***');
+define('DB_PASSWORD', '');
 
 /** MySQL hostname */
-//define('DB_HOST', 'localhost');
-define('DB_HOST', '***REMOVED***');
+define('DB_HOST', '');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
